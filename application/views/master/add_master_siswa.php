41e881a1a55a435e2049c72d18b14c474f36556e
naufal94 <muhammadnaufalislam94@gmail.com>
2015-04-03 20:27:03 +0700

    naufal

--- application/views/master/add_master_siswa.php
+++ application/views/master/add_master_siswa.php
@@ -22,7 +22,7 @@
                            <span class="divider">/</span>
                        </li>
                        <li class="active">
-                           Tambah Data Naufal
+                           Tambah Data Siswa
                        </li>
                        <li class="pull-right search-wrap">
                            <form action="http://thevectorlab.net/metrolab/search_result.html" class="hidden-phone">
