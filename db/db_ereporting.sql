91d2f81f375fc44474420ad7cac54acce92bf199
unknown <syabab45@gmail.com>
2015-04-03 17:44:17 +0700

    add db

---
+++ db/db_ereporting.sql
@@ -0,0 +1,73 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.1.33-community : Database - db_ereporting
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_ereporting` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `db_ereporting`;
+
+/*Table structure for table `tbl_karyawan` */
+
+DROP TABLE IF EXISTS `tbl_karyawan`;
+
+CREATE TABLE `tbl_karyawan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nik` varchar(50) DEFAULT NULL,
+  `nama_karyawan` varchar(50) DEFAULT NULL,
+  `jabatan` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `PRI` (`nik`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tbl_karyawan` */
+
+insert  into `tbl_karyawan`(`id`,`nik`,`nama_karyawan`,`jabatan`) values (1,'1234567890','Fata Aisy Salim','Guru'),(2,'0987654321','Muhief Muhfianto','Wakasek'),(3,'123213213','Agus Hariyanto','Kepsek'),(4,'456745676','Naufal Islam','Ketua Yayasan');
+
+/*Table structure for table `tbl_siswa` */
+
+DROP TABLE IF EXISTS `tbl_siswa`;
+
+CREATE TABLE `tbl_siswa` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nis` varchar(50) DEFAULT NULL,
+  `nama_siswa` varchar(50) DEFAULT NULL,
+  `jns_kel` varchar(10) DEFAULT NULL,
+  `kelas` varchar(10) DEFAULT NULL,
+  `jurusan` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Data for the table `tbl_siswa` */
+
+/*Table structure for table `tbl_user` */
+
+DROP TABLE IF EXISTS `tbl_user`;
+
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nik` varchar(50) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `password_plain` varchar(100) DEFAULT NULL,
+  `user_group` int(2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tbl_user` */
+
+insert  into `tbl_user`(`id`,`nik`,`username`,`password`,`password_plain`,`user_group`) values (1,'1234567890','admin','e10adc3949ba59abbe56e057f20f883e','123456',1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
